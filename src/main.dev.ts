1d19ee4fbf0e0f6d78e8d709beeedbede426a49b
Dawid NPM8 Malysa <nnnp.dawid@gmail.com>
2021-01-15 22:34:39 +0100

    Change default window size
    Window was sized in 4:3 ratio which is worst then 16:9.

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -69,8 +69,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    width: 1280,
+    height: 720,
     icon: getAssetPath('icon.svg'),
     webPreferences: {
       nodeIntegration: true,
