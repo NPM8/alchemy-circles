3f324e83a2a9c98a583da30662aa91e44521a7c6
Dawid NPM8 Malysa <nnnp.dawid@gmail.com>
2021-01-15 20:48:23 +0100

    Update title and icon

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -71,7 +71,7 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
-    icon: getAssetPath('icon.png'),
+    icon: getAssetPath('icon.svg'),
     webPreferences: {
       nodeIntegration: true,
     },
