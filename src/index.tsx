80e038e3fb4a59f3a288b758e4e8a2909b4070f4
Dawid NPM8 Malysa <nnnp.dawid@gmail.com>
2021-02-04 15:26:01 +0100

    [feat] Change manu and title designe

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-// import './App.glbal.css';
-import 'antd/dist/antd.css';
+import './App.global.css';
+// import 'antd/dist/antd.css';
 
 render(<App />, document.getElementById('root'));
